af1a8507dbd480cd3ffe7125b577b82c23498545
Avikalpa Kundu <avi@gour.top>
2026-08-31 13:11:18 +0530

    ymacs: make build portable for CI (relative ASDF registry)
    Fix build.lisp absolute /home/pi path that broke GitHub Actions
    Build Binary job (Component :YMACS not found on runner). Use
    relative ./ and ./src/ so CI and local both work.
    
    Signed-off-by: Avikalpa Kundu <avi@gour.top>

--- build.lisp
+++ build.lisp
@@ -1,8 +1,8 @@
 ;;;; build.lisp --- Produce ymacs binary image via SBCL save-lisp-and-die
 (require :asdf)
 (require :sb-bsd-sockets)
-(push #P"/home/pi/gh/ymacs/" asdf:*central-registry*)
-(push #P"/home/pi/gh/ymacs/src/" asdf:*central-registry*)
+(push #P"./" asdf:*central-registry*)
+(push #P"./src/" asdf:*central-registry*)
 (asdf:load-system :ymacs)
 (format t "~&[build] Loaded ymacs ~a~%" (asdf:component-version (asdf:find-system :ymacs)))
 (format t "[build] Dumping image to ymacs-bin~%")
